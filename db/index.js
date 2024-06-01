29a5fdf4133137514390d505e5e8ea8fdf572557
Satinder Singh <kainth00013@gmail.com>
2024-06-01 05:30:54 -0400

    DB configuration is done and ES6 import configuration is added

---
+++ db/index.js
@@ -0,0 +1,14 @@
+import mongoose from 'mongoose'
+
+const connectDB = async () =>{
+    try {
+        const connectionInstance = await mongoose.connect(`${process.env.DB_URL}/${process.env.DB_NAME}`)
+        console.log(`DB is connnected`);
+        console.log(`Connection instance : `)
+        console.dir(connectionInstance.connection.host);
+    } catch (error) {
+        console.log(`Connection failed for mongodb : `,error); 
+    }
+}
+
+export {connectDB}
--- index.js
+++ index.js
@@ -1,12 +1,18 @@
-const express= require('express')
+import express from 'express'
+import dotenv from 'dotenv'
+import {connectDB} from './db/index.js';
+
 const app = express()
+dotenv.config()
 
-app.listen(3000, ()=>{
-    console.log(`Application is listening on port 3000`);
+const port = process.env.PORT || 4000;
+
+connectDB()
+.then(()=>{
+    app.listen(port,()=>{
+        console.log(`App is started and server is running at port : ${port}`);
+    })
 })
-
-app.get('/',(req,res)=>{
-    console.log("hello world");
-    res.send("hello world")
+.catch((err)=>{
+    console.log(`An error occured while starting the app : `,err);
 })
-
